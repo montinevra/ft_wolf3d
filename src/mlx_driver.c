65f3a973cee3c1161e09b262df0d752418afaf4b
Paolo Van Erp <paolo.vanerp@gmail.com>
2017-05-06 15:49:26 -0700

    img initialization is now a function

--- src/mlx_driver.c
+++ src/mlx_driver.c
@@ -2,14 +2,38 @@
 #include "mlx_handlers.h"
 #include "wolf3d.h"														//debuf
 
-static int	is_big_endian(void)
+static int			is_big_endian(void)
 {
 	const unsigned int	end = 1;
 
 	return (*(((char *)&end) + 3));
 }
 
-void		mlx_driver(t_mlx mlx)
+// static void			bg_init(t_mlx_img *img, t_mlx mlx)
+// {
+// 	int	i;
+
+// 	i = ~0;
+// 	while (++i < mlx.wsize.x * mlx.wsize.y)
+// 	{
+// 		img->start[i] = mlx_get_color_value(mlx.id, 0x777777);
+// 	}
+// }
+
+static t_mlx_img	img_init(t_mlx mlx)
+{
+	t_mlx_img	img;
+
+	img.id = mlx_new_image(mlx.id, mlx.wsize.x, mlx.wsize.y);
+	img.bppx = 32;
+	img.lsize = mlx.wsize.y * img.bppx / 4;
+	img.endian = is_big_endian();
+	img.start = mlx_get_data_addr(img.id, &img.bppx, &img.lsize,
+		&img.endian);
+	return (img);
+}
+
+void				mlx_driver(t_mlx mlx)
 {
 	char	*wname;
 	int 	i;
@@ -18,18 +42,10 @@ void		mlx_driver(t_mlx mlx)
 	mlx.id = mlx_init();
 	mlx.win = mlx_new_window(mlx.id, mlx.wsize.x, mlx.wsize.y, wname);
 	ft_strdel(&wname);
-	mlx.img.id = mlx_new_image(mlx.id, mlx.wsize.x, mlx.wsize.y);
-	mlx.img.bppx = 32;
-	mlx.img.lsize = mlx.wsize.y * mlx.img.bppx / 4;
-	mlx.img.endian = is_big_endian();
-	mlx.img.start = mlx_get_data_addr(mlx.img.id, &mlx.img.bppx, &mlx.img.lsize,
-		&mlx.img.endian);
-	mlx.fog.id = mlx_new_image(mlx.id, mlx.wsize.x, mlx.wsize.y);
-	mlx.fog.bppx = 32;
-	mlx.fog.lsize = mlx.wsize.y * mlx.fog.bppx / 4;
-	mlx.fog.endian = is_big_endian();
-	mlx.fog.start = mlx_get_data_addr(mlx.fog.id, &mlx.fog.bppx, &mlx.fog.lsize,
-		&mlx.fog.endian);
+	mlx.img = img_init(mlx);
+	mlx.fog = img_init(mlx);
+	// mlx.bg = img_init(mlx);
+	// bg_init(&mlx.bg, mlx);
 	i = ~0;
 	while (++i < MAXKEYS)
 		mlx.keys[i] = 0;
@@ -44,7 +60,6 @@ void		mlx_driver(t_mlx mlx)
 	mlx_expose_hook(mlx.win, expose_hook, &mlx);
 	mlx_hook(mlx.win, 2, 0, key_press_hook, &mlx);
 	mlx_hook(mlx.win, 3, 0, key_release_hook, &mlx);
-	// if (ft_strequ(((t_frac *)mlx.data)->name, "julia"))
 	// mlx_hook(mlx.win, 6, 0, motion_hook, &mlx);
 	mlx_hook(mlx.win, 17, 0, exit_hook, &mlx);
 	mlx_do_key_autorepeatoff(mlx.id);
