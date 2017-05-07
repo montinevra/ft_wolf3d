af9b3ee0dfa298ccecd3c3f70d57e4eee51a275c
Paolo Van Erp <paolo.vanerp@gmail.com>
2017-05-07 15:16:29 -0700

    final pre-norm push

--- src/more_math.c
+++ src/more_math.c
@@ -10,13 +10,9 @@ t_vec	add_vector(t_vec a, double dir, double mag)
 	double	by;
 
 	ax = cos(a.dir) * a.mag;
-	// printf("ax = %f\n", ax);
 	ay = sin(a.dir) * a.mag;
-	// printf("ay = %f\n", ay);
 	bx = cos(dir) * mag;
-	// printf("bx = %f\n", bx);
 	by = sin(dir) * mag;
-	// printf("by = %f\n", by);
 	c.dir = atan2(ay + by, ax + bx);
 	c.mag = hypot(ax + bx, ay + by);
 	return (c);
--- src/wolf3d.h
+++ src/wolf3d.h
@@ -1,26 +1,11 @@
 #ifndef WOLF3D_H
 # define WOLF3D_H
+# include <stdio.h>
 # include <unistd.h>
 # include <math.h>
 # include "../libft/libft.h"
 # include "more_math.h"
 # include "mlx_driver.h"
-#include <stdio.h>															//debug
-
-// # define M_TAU M_PI * 2.0 
-// # define MAXKEYS 279
-
-// typedef struct	s_point
-// {
-// 	int	x;
-// 	int	y;
-// }				t_point;
-
-// typedef struct	s_map
-// {
-// 	char	**map;
-// 	t_point	start;
-// }				t_map;
 
 typedef struct	s_pos
 {
@@ -36,22 +21,12 @@ typedef	struct	s_plr
 	double	rot;
 }				t_plr;
 
-typedef struct	s_clrs
-{
-	int	n[4];
-	int	s[4];
-	int	e[4];
-	int	w[4];
-}				t_clrs;
-
 typedef struct	s_wld
 {
 	char		**map;
 	char		*name;
 	t_coord		size;
-	t_clrs		clrs;
 	t_plr		plr;
-	// int			keys[MAXKEYS];
 }				t_wld;
 
 typedef struct	s_ray
