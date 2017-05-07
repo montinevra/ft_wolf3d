75d96a4b6df281ecbf94459b7ea91e0f96ea828b
Paolo Van Erp <paolo.vanerp@gmail.com>
2017-05-07 15:36:14 -0700

    moved drawing code to helper function

--- src/draw.c
+++ src/draw.c
@@ -1,81 +1,48 @@
 #include "wolf3d.h"
 #include <stdio.h>														///debug
 
+static void	draw_col(t_col col, int x, t_mlx *mlx)
+{
+	int				y;
+	unsigned int	ccolor;
+	unsigned int	fcolor;
+	unsigned int	flrfog;
+
+	flrfog = mlx_get_color_value(mlx->id, 0xFF000000);
+	ccolor = mlx_get_color_value(mlx->id, 0x7777FF);
+	fcolor = mlx_get_color_value(mlx->id, 0x335533);
+	y = ~0;
+	while (++y < mlx->wsize.y / 2 - col.height)
+	{
+		*((int *)(mlx->img.start + (y) * mlx->img.lsize) + x) = ccolor;
+		*((int *)(mlx->img.start + (mlx->wsize.y - y - 1) * mlx->img.lsize) + x) = fcolor;
+		*((int *)(mlx->fog.start + (y) * mlx->fog.lsize) + x) = flrfog;
+		*((int *)(mlx->fog.start + (mlx->wsize.y - y - 1) * mlx->fog.lsize) + x) = flrfog;
+	}
+	while (y < mlx->wsize.y / 2)
+	{
+		*((int *)(mlx->img.start + (y) * mlx->img.lsize) + x) = col.color;
+		*((int *)(mlx->img.start + (y + col.height) * mlx->img.lsize) + x) = col.color;
+		*((int *)(mlx->fog.start + (y) * mlx->fog.lsize) + x) = col.fogclr;
+		*((int *)(mlx->fog.start + (y + col.height) * mlx->fog.lsize) + x) = col.fogclr;
+		y++;
+	}
+}
+
 static void	*draw_thread(void *mlxt)
 {
 	t_mlx		*mlx;
 	t_coord	px;
-	unsigned int			ccolor;
-	unsigned int			fcolor;
 	t_col		col;
-	// int lines = 0 ;
 
 	mlx = ((t_mlx_thread *)mlxt)->mlx;
 	px.x = ((t_mlx_thread *)mlxt)->y;
-	// t_wld *wld= (t_wld *)mlx->data;
-	// printf("in thread %d\n", px.x);																///deubg
-
-	// printf("mlxt = %p, mlx = %p; img.start = %p; img.end = %p\n", mlxt, mlx, mlx->img.start, mlx->img.start + mlx->wsize.y * mlx->img.lsize);								//debug
 	while (px.x < mlx->wsize.x)
 	{
-		// printf("thread = %d ", px.x % 4);
 		col = get_column(px.x, mlx);
-
-		// col.height = mlx->wsize.y / 2;
-		ccolor = mlx_get_color_value(mlx->id, 0x7777FF);
-		fcolor = mlx_get_color_value(mlx->id, 0x335533);
-		px.y = ~0;
-
-
-		while (++px.y < mlx->wsize.y / 2 - col.height)
-		{
-			// ccolor = 0xFF0000 >> (px.x % THREADS * 8);
-			// printf("ccolor = %d; threads = %d\n", ccolor, THREADS);
-			*((int *)(mlx->img.start + (px.y) * mlx->img.lsize) + px.x) = ccolor;
-			// printf("fcolor = %d; threads = %d\n", fcolor, THREADS);
-			*((int *)(mlx->img.start + (mlx->wsize.y - px.y - 1) * mlx->img.lsize) + px.x) = fcolor;
-
-			// float angle = atan2(px.x - mlx->wsize.x / 2, mlx->wsize.x / tan(wld->plr.fov.x / 2));
-			// float dist = (mlx->wsize.y / 2) / px.y / cos(angle) / tan(wld->plr.fov.y / 2);
-			// int flrclr = mlx_get_color_value(mlx->id, 0 - ((int)(0x08 * dist) << 24));
-			int flrclr = mlx_get_color_value(mlx->id, 0xFF000000);
-
-
-			*((int *)(mlx->fog.start + (px.y) * mlx->fog.lsize) + px.x) = flrclr;
-			*((int *)(mlx->fog.start + (mlx->wsize.y - px.y - 1) * mlx->fog.lsize) + px.x) = flrclr;
-			// if (!(px.y % (mlx->wsize.y / 200)))
-			// {
-			// 	ccolor -= mlx_get_color_value(mlx->id, 0x010101);
-			// 	fcolor -= mlx_get_color_value(mlx->id, 0x010101);
-			// }
-		}
-		// *(int *)(mlx->img.start + (px.y) * mlx->img.lsize + px.x * 4) = 0;
-		// *(int *)(mlx->img.start + (mlx->wsize.y - px.y) * mlx->img.lsize + px.x * 4) = 0;
-		while (px.y < mlx->wsize.y / 2)
-		{
-			// color = 0xFF0000 >> (px.x % THREADS * 8);
-			// printf("color = %d; threads = %d\n", color, THREADS);
-			*((int *)(mlx->img.start + (px.y) * mlx->img.lsize) + px.x) = col.color;
-			*((int *)(mlx->img.start + (px.y + col.height) * mlx->img.lsize) + px.x) = col.color;
-			*((int *)(mlx->fog.start + (px.y) * mlx->fog.lsize) + px.x) = col.fogclr;
-			*((int *)(mlx->fog.start + (px.y + col.height) * mlx->fog.lsize) + px.x) = col.fogclr;
-			// *(int *)(mlx->img.start + (mlx->wsize.y - px.y) * mlx->img.lsize + px.x * 4) = col.color;
-			px.y++;
-		}
-		// *(int *)(mlx->img.start + (px.y) * mlx->img.lsize + px.x * 4) = col.color;
-
-
-		// while (++px.y < mlx->wsize.y)
-		// {
-		// 	// color = 0xFF0000 >> (px.x % THREADS * 8);
-		// 	color = 0x777777;
-		// 	// printf("color = %d; threads = %d\n", color, THREADS);
-		// 	*(int *)(mlx->img.start + px.y * mlx->img.lsize + px.x * 4) = color;
-		// }
+		draw_col(col, px.x, mlx);
 		px.x += THREADS;
-		// lines++;
 	}
-	// printf("lines drawn: %d\n", lines);
 	return (NULL);
 }
 
